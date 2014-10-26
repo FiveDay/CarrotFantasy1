252439dd8a69e40b61b2dbf23e2c80abf91a89eb
zhangyu528 <zhangyu528@163.com>
2014-10-26 20:49:10 +0800

    1.add *.unityproj to gitignore 2.make a mainscene 's ui root prefab 3.test addcomponentMenu func in compo1.cs

---
+++ Assets/zy/Component/compo1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("zy/compo1")]
+public class compo1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
