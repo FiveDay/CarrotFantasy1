e55cebf620faa906596b99dadb70107e1e289918
zhangyu528 <zhangyu528@163.com>
2014-12-05 00:19:18 +0800

    1.添加loading场景 2.异步加载场景，并协同。

---
+++ Assets/zy/UI/script/Loading.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadScene ());
+	}
+
+	IEnumerator loadScene()
+	{
+		AsyncOperation async = Application.LoadLevelAsync (2);
+		yield return async;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
